11a9701cbd4637908659727214828d842fac83f4
Pratham-Patel1 <pratham.s.patel01@gmail.com>
2025-04-19 16:16:32 +0530

    Switchcaseadded

--- main.cpp
+++ main.cpp
@@ -10,23 +10,38 @@ class Flights {
     public :
     string FlightID;
     string Origin;
+    string OriginSF;
     string Destination;
+    string DestinationSF;
     string DepartureTime;
-    string ArrivalTime;
+    string ArrivalTime; 
 
 
     void display() const {
-        cout << "Flight ID: " << FlightID << ", Origin: " << Origin
-             << ", Destination: " << Destination << ", Departure Time: " << DepartureTime
-             << ", Arrival Time: " << ArrivalTime << endl;
+        cout<<"\n Available Flights::"<<endl;
+
+        cout << "Flight ID: " << FlightID << ", Origin: " << Origin << " (" << OriginSF << ")" 
+        << ", Destination: " << Destination << " " << DestinationSF << ", Departure Time: " << DepartureTime
+        << ", Arrival Time: " << ArrivalTime << endl;
     }
 
 };
 
 int main() {
-    ifstream file("flights.txt");
     vector<Flights> flights;
+    int n;
+    
+    cout<<"Enter 1 For View Flights"<<endl;
+    cout<<"Enter 2 For View Tickets"<<endl;
+    cout<<"Enter 0 For Exit.."<<endl;
 
+while(true){
+    switch(n){
+ case 1:
+ char ch;int ps;
+ ifstream file("flights.txt");
+
+    cout << "Flight Booking" << endl;
     if (!file.is_open()) {
         cout << "Error: Could not open file." << endl;
         return 1;
@@ -40,8 +55,8 @@ int main() {
 
 
     Flights temp;
-    while (file >> temp.FlightID >> temp.Origin >> temp.Destination >> temp.DepartureTime >> temp.ArrivalTime) {
-        if (temp.Origin == searchOrigin && temp.Destination == searchDestination) {
+    while (file >> temp.FlightID >> temp.Origin >> temp.OriginSF >> temp.Destination >> temp.DestinationSF >> temp.DepartureTime >> temp.ArrivalTime) {
+        if ((temp.Origin == searchOrigin || temp.OriginSF == searchOrigin) && temp.Destination == searchDestination) {
             flights.push_back(temp);
         }
     }
@@ -49,12 +64,32 @@ int main() {
     if (flights.empty()) {
         cout << "Flight not found." << endl;
     } else {
-        cout << "Matching flight(s):\n";
+        cout << "Matching result(s):\n";
         for (const auto &f : flights) {
             f.display();
         }
     }
+ file.close();
+ cout<<"Do You Want Book Flight- If Yes Press 'Y' Or if Not Press 'N'";
+ cin>>ch;
+ cout<<"Enter Number OF Passenger::";
+ cin>>ps;
+ if(ch=='y'|| ch=='Y')
+ {
+    int i=0;
+ while(ps!=0){
+    string name;int age;
+    cout<<"Enter Details Of Passener"<<i+1;
+    cin>>
 
-    file.close();
+ }
+ }
+
+
+
+
+    }
+}
+
     return 0;
 }
